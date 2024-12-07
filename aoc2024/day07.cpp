1e1c0ac418c25a324f183e858371fdb9efdb9acb
Jason Feinstein <jwf@google.com>
2024-12-07 15:52:33 -0800

    5x improvement for part 2, no more string manipulation

--- aoc2024/day07.cpp
+++ aoc2024/day07.cpp
@@ -1,9 +1,7 @@
 #include <fstream>
-#include <regex>
 #include <string>
-#include <iostream>
 #include <vector>
-#include <cstdint>
+#include <chrono>
 
 #include "absl/strings/str_split.h"
 #include "tools/log.h"
@@ -22,16 +20,22 @@ bool solve(ull target, std::vector<ull>& terms, int index, ull soFar) {
 bool solve2(ull target, std::vector<ull>& terms, int index, ull soFar) {
     if (index == terms.size()) return target == soFar;
     if (soFar > target) return false;
+    ull multiplier = 1;
+    ull toShift = terms[index];
+    while (toShift > 0) {
+        toShift /= 10;
+        multiplier *= 10;
+    }
     return solve2(target, terms, index + 1, soFar + terms[index])
         || solve2(target, terms, index + 1, soFar * terms[index])
-        || solve2(target, terms, index + 1, std::stoull(std::to_string(soFar) + std::to_string(terms[index])));
+        || solve2(target, terms, index + 1, soFar * multiplier + terms[index]);
 }
 
 void part1(std::ifstream& in) {
     LOG_LEVEL(INFO_LEVEL);
     std::string line;
     ull result = 0;
-    auto time = std::chrono::system_clock::now();
+    auto before = std::chrono::high_resolution_clock::now();
     while (std::getline(in, line)) {
         std::vector<std::string> colonParts = absl::StrSplit(line, ": ");
         ull target = std::stoull(colonParts[0]);
@@ -47,15 +51,16 @@ void part1(std::ifstream& in) {
             result += target;
         }
     }
-    auto duration = std::chrono::system_clock::now() - time;
-    INFO("Part 1: %lld, duration: %ld", result, duration);
+    auto after = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
+    INFO("Part 1: %lld, duration: %lld us", result, duration.count());
 }
 
 void part2(std::ifstream& in) {
     LOG_LEVEL(INFO_LEVEL);
     std::string line;
     ull result = 0;
-    auto time = std::chrono::system_clock::now();
+    auto before = std::chrono::high_resolution_clock::now();
     while (std::getline(in, line)) {
         std::vector<std::string> colonParts = absl::StrSplit(line, ": ");
         ull target = std::stoull(colonParts[0]);
@@ -70,8 +75,9 @@ void part2(std::ifstream& in) {
             result += target;
         }
     }
-    auto duration = std::chrono::system_clock::now() - time;
-    INFO("Part 2: %lld, duration: %d", result, duration);
+    auto after = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
+    INFO("Part 2: %lld, duration: %lld us", result, duration.count());
 }
 
 MAIN(part1, part2);
